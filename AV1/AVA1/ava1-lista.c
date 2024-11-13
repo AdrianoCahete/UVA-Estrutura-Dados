d4e305a0037b38fbfa4ce39eb0a7c0bb8388680c
Adriano Cahete <me@adrianocahete.dev>
2024-11-12 23:12:57 -0300

    feat(app): adicionando opções pq pretendo dormir e amanha eu vejo

--- AV1/AVA1/ava1-lista.c
+++ AV1/AVA1/ava1-lista.c
@@ -1,6 +1,26 @@
+// Também disponivel em https://github.com/AdrianoCahete/UVA-Estrutura-Dados :)
+
 #include <stdio.h>
 
-// Função principal
-int main() {
-    
+// Main
+int main()
+{
+  int option;
+
+  do
+  {
+    printf("\nEscolha uma operação:\n");
+
+    printf("1. Inserir\n");
+    printf("2. Consultar\n");
+    printf("3. Alterar\n");
+    printf("4. Remover\n");
+    printf("5. Listar\n");
+
+    printf("\n0. Sair\n");
+    printf("Opcao: ");
+    scanf("%d", &option);
+  } while (option != 6);
+
+  return 0;
 }
--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 ## AVA1
 
-TBD
+### Compilar
+
+```bash
+gcc AV1\AVA1\ava1-lista.c -o ava1-lista.exe
+```
+
+Ou rodar a task `tasks: Run build task` no VSCode, usando o `gcc` como compilador (é necessário estar com o arquivo `ava1-lista.c` aberto e em foco).
 
 ## AVA2
 
