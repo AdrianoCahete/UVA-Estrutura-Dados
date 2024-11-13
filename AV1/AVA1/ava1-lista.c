aa26100d151ebd8c31e9f0fcc58e8b57719002dd
Adriano Cahete <me@adrianocahete.dev>
2024-11-12 23:33:03 -0300

    feat(app): melhorando a visualização + removendo task duplicada

--- AV1/AVA1/ava1-lista.c
+++ AV1/AVA1/ava1-lista.c
@@ -5,21 +5,31 @@
 // Main
 int main()
 {
+  // Estrutura do nó
+  typedef struct Node
+  {
+    int data;
+    struct Node *next;
+  } Node;
+
   int option;
 
   do
   {
-    printf("\nEscolha uma operação:\n");
+    printf("\nEscolha uma operacao:\n\n");
 
-    printf("1. Inserir\n");
-    printf("2. Consultar\n");
-    printf("3. Alterar\n");
-    printf("4. Remover\n");
-    printf("5. Listar\n");
+    printf("[ 1 ] Inserir\n");
+    printf("[ 2 ] Consultar\n");
+    printf("[ 3 ] Alterar\n");
+    printf("[ 4 ] Remover\n");
+    printf("[ 5 ] Listar\n");
 
-    printf("\n0. Sair\n");
+    printf("\n[ 0 ] Sair\n");
+    printf("---------------------------\n");
     printf("Opcao: ");
     scanf("%d", &option);
+    printf("---------------------------\n");
+
   } while (option != 6);
 
   return 0;
