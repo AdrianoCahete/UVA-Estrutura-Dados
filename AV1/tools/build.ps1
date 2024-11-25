53c31c5d79fdb66bf3e10f718e86038dab137986
Adriano Cahete <me@adrianocahete.dev>
2024-11-25 15:12:21 -0300

    feat(chore): adicionando arquivo de build e run pra powershell

---
+++ AV1/tools/build.ps1
@@ -0,0 +1,8 @@
+# TODO: Buildar qual?
+
+# AVA1
+# gcc AV1\AVA1\ava1-lista.c -o .\bin\ava1-lista.exe
+
+# AVA2
+gcc AV1\AVA2\ava2-busca-arvore.c -o AV1\AVA2\bin\ava2-busca.exe
+
---
+++ AV1/tools/run.ps1
@@ -0,0 +1,2 @@
+# TODO: Rodar qual?
+& .\AV1\AVA2\bin\ava2-busca.exe
