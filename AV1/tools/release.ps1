a7b3d7e4e4859f87893d84c25c084373f3c79993
Adriano Cahete <me@adrianocahete.dev>
2024-11-25 19:55:32 -0300

    feat(chore): add arquivo de automacao de release

---
+++ AV1/tools/release.ps1
@@ -0,0 +1,2 @@
+& Compress-Archive -Path .\AV1\AVA1\* -DestinationPath .\AV1\release\ava1-lista__Adriano-Cahete.zip -Force ;
+& Compress-Archive -Path .\AV1\AVA2\* -DestinationPath .\AV1\release\ava2-busca-arvore__Adriano-Cahete.zip -Force
