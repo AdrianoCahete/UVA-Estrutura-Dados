47c51c10becf4c1952dfd09f97ea420f245150ad
Adriano Cahete <me@adrianocahete.dev>
2024-11-25 20:19:56 -0300

    feat(chore): add arquivo de automacao de release

--- AV1/tools/release.ps1
+++ AV1/tools/release.ps1
@@ -1,2 +1,3 @@
 & Compress-Archive -Path .\AV1\AVA1\* -DestinationPath .\AV1\release\ava1-lista__Adriano-Cahete.zip -Force ;
 & Compress-Archive -Path .\AV1\AVA2\* -DestinationPath .\AV1\release\ava2-busca-arvore__Adriano-Cahete.zip -Force
+echo "[ UVA - Estrutura de Dados ] AV1 (AVA1+AVA2) - Adriano Cahete <https://github.com/AdrianoCahete/UVA-Estrutura-Dados>"
